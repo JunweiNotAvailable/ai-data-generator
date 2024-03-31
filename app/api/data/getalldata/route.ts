849daaf53b4d06efb2516d7b826c1e77dd95008c
Junwei <wertycxx@gmail.com>
2024-03-31 23:00:02 +0800

    debug

--- app/api/data/getalldata/route.ts
+++ app/api/data/getalldata/route.ts
@@ -2,17 +2,11 @@ import { getAllData } from "@/app/utils/postgre";
 import { NextResponse } from "next/server";
 
 export async function GET (request: Request) {
-  try {
-    const userEmail = new URL(request.url).searchParams.get('userEmail');
-    if (userEmail) {
-      console.log(userEmail);
-      const result = await getAllData(userEmail);
-      console.log(result);
-      return new NextResponse(JSON.stringify(result))
-    } else {
-      return new NextResponse(JSON.stringify({ error: "Failed getting params" }))
-    }
-  } catch (error) {
-    return new NextResponse(JSON.stringify({ error: 'Failed getting data' }))
+  const userEmail = new URL(request.url).searchParams.get('userEmail');
+  if (userEmail) {
+    const result = await getAllData(userEmail);
+    return new NextResponse(JSON.stringify(result))
+  } else {
+    return new NextResponse(JSON.stringify({ error: "Failed getting params" }))
   }
 }
