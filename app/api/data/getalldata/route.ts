347f4bd3d5165616023f7b2168dd4f70a32e2c62
Junwei <wertycxx@gmail.com>
2024-03-31 22:57:51 +0800

    debug

--- app/api/data/getalldata/route.ts
+++ app/api/data/getalldata/route.ts
@@ -5,9 +5,10 @@ export async function GET (request: Request) {
   try {
     const userEmail = new URL(request.url).searchParams.get('userEmail');
     if (userEmail) {
+      console.log(userEmail);
       const result = await getAllData(userEmail);
       console.log(result);
-      return new Response(JSON.stringify(result))
+      return new NextResponse(JSON.stringify(result))
     } else {
       return new NextResponse(JSON.stringify({ error: "Failed getting params" }))
     }
